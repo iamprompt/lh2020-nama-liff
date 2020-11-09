7e4ae358b0e0a39b03283f16de356bf5083c1460
Supakarn Laorattanakul <wearedprompt@gmail.com>
2020-11-09 15:38:13 +0700

    Add Form Icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import customIcons from './assets/images/customIcons.js'
+
 export default {
   target: 'static',
 
@@ -38,7 +40,7 @@ export default {
       autoImport: true,
     },
     icons: {
-      extend: {},
+      extend: { ...customIcons },
     },
   },
 
--- pages/appointment/create.vue
+++ pages/appointment/create.vue
@@ -52,6 +52,9 @@
       <CGrid template-columns="60% auto" gap="10px" mb="4">
         <!-- Date -->
         <CInputGroup>
+          <CInputLeftElement>
+            <CIcon name="icalendar" color="#FF9A3D" />
+          </CInputLeftElement>
           <CInput
             v-model="eventForm.eventDate"
             type="date"
@@ -62,6 +65,9 @@
         </CInputGroup>
         <!-- Time -->
         <CInputGroup>
+          <CInputLeftElement>
+            <CIcon name="iclock" color="#FF9A3D" />
+          </CInputLeftElement>
           <CInput
             v-model="eventForm.eventTime"
             type="time"
@@ -306,5 +312,29 @@ export default Vue.extend({
 .choiceBox {
   box-shadow: 0px 3px 20px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
+}
+</style>
+
+<style lang="scss">
+input[type='date']::-webkit-calendar-picker-indicator {
+  color: rgba(0, 0, 0, 0);
+  opacity: 1;
+  background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.9062 1.875H18.75V0.75C18.75 0.335812 18.4142 0 18 0C17.5858 0 17.25 0.335812 17.25 0.75V1.875H6.75V0.75C6.75 0.335812 6.41423 0 6 0C5.58577 0 5.25 0.335812 5.25 0.75V1.875H3.09375C1.38783 1.875 0 3.26283 0 4.96875V20.9062C0 22.6122 1.38783 24 3.09375 24H20.9062C22.6122 24 24 22.6122 24 20.9062V4.96875C24 3.26283 22.6122 1.875 20.9062 1.875ZM22.5 20.9062C22.5 21.7865 21.7865 22.5 20.9062 22.5H3.09375C2.21353 22.5 1.5 21.7865 1.5 20.9062V8.48438C1.5 8.35495 1.60495 8.25 1.73438 8.25H22.2656C22.395 8.25 22.5 8.35495 22.5 8.48438V20.9062Z' fill='url(%23paint0_linear)'/%3E%3Cdefs%3E%3ClinearGradient id='paint0_linear' x1='12' y1='0' x2='12' y2='24' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0.166667' stop-color='%23FF9A3D'/%3E%3Cstop offset='1' stop-color='%23F06129'/%3E%3C/linearGradient%3E%3C/defs%3E%3C/svg%3E%0A");
+  width: 18px;
+  height: 18px;
+  cursor: pointer;
+  border-radius: 50%;
+  margin-left: 0.5rem;
+}
+
+input[type='time']::-webkit-calendar-picker-indicator {
+  color: rgba(0, 0, 0, 0);
+  opacity: 1;
+  background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 0C5.38293 0 0 5.38293 0 12C0 18.6171 5.38293 24 12 24C18.6171 24 24 18.6171 24 12C24 5.38293 18.6171 0 12 0ZM17.707 18.2069C17.512 18.4019 17.256 18.5001 17.0001 18.5001C16.7441 18.5001 16.4879 18.4019 16.2931 18.2069L11.293 13.207C11.105 13.0201 11.0001 12.7659 11.0001 12.5001V6C11.0001 5.44702 11.4479 5.00006 12 5.00006C12.5521 5.00006 12.9999 5.44702 12.9999 6V12.0861L17.707 16.793C18.098 17.1841 18.098 17.816 17.707 18.2069Z' fill='url(%23paint0_linear)'/%3E%3Cdefs%3E%3ClinearGradient id='paint0_linear' x1='12' y1='0' x2='12' y2='24' gradientUnits='userSpaceOnUse'%3E%3Cstop stop-color='%23F06129'/%3E%3Cstop offset='1' stop-color='%23FF9A3D'/%3E%3C/linearGradient%3E%3C/defs%3E%3C/svg%3E%0A");
+  width: 18px;
+  height: 18px;
+  cursor: pointer;
+  border-radius: 50%;
+  margin-left: 0.5rem;
 }
 </style>
