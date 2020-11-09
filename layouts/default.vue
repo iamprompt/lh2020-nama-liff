6e3d0b54bdd672046405329441262dd784845604
Supakarn Laorattanakul <wearedprompt@gmail.com>
2020-11-09 14:54:00 +0700

    Add Webfont

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,21 +10,3 @@
     </CThemeProvider>
   </div>
 </template>
-<script>
-import {
-  CThemeProvider,
-  CColorModeProvider,
-  CReset,
-  CBox,
-} from '@chakra-ui/vue'
-
-export default {
-  name: 'App',
-  components: {
-    CThemeProvider,
-    CColorModeProvider,
-    CReset,
-    CBox,
-  },
-}
-</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,12 @@
 export default {
-  // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
-  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'liff',
+    titleTemplate: (title) => {
+      return title
+        ? `${title} | NAMA : The Assistant Bot`
+        : 'NAMA : The Assistant Bot'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -13,34 +15,33 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['~assets/styles/css/webfont.css'],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-  ],
+  buildModules: ['@nuxt/typescript-build'],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/chakra
-    '@chakra-ui/nuxt',
-    // https://go.nuxtjs.dev/emotion
-    '@nuxtjs/emotion',
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
+  modules: ['@chakra-ui/nuxt', '@nuxtjs/emotion', '@nuxtjs/axios'],
 
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  chakra: {
+    extendTheme: {
+      fonts: {
+        heading:
+          '"SF Pro TH Display", "SF Pro Display", "Helvetica Neue", "Helvetica", "Arial", sans-serif',
+        body:
+          '"SF Pro Display", "Thonburi", "Helvetica Neue", "Helvetica", "Arial", sans-serif',
+      },
+    },
+    config: {
+      autoImport: true,
+    },
+    icons: {
+      extend: {},
+    },
+  },
+
   axios: {},
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
 }
