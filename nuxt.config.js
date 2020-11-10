d9c6bf33963bb044f567ce5e6ac3191e5d4ceb79
Supakarn Laorattanakul <wearedprompt@gmail.com>
2020-11-10 14:57:52 +0700

    Add VConsole

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,7 @@
 import customIcons from './assets/images/customIcons.js'
 
+require('dotenv').config()
+
 export default {
   target: 'static',
   ssr: false,
@@ -18,7 +20,7 @@ export default {
 
   css: ['~assets/styles/css/webfont.css'],
 
-  plugins: [],
+  plugins: [{ src: '~/plugins/vconsole.js', ssr: false }],
 
   components: true,
 
@@ -29,6 +31,7 @@ export default {
     '@nuxtjs/emotion',
     '@nuxtjs/axios',
     '@nuxtjs/dayjs',
+    '@nuxtjs/firebase',
   ],
 
   chakra: {
@@ -53,6 +56,27 @@ export default {
     locales: ['en', 'th'],
     defaultLocale: 'th',
     plugins: [], // Your Day.js plugin
+  },
+
+  firebase: {
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    },
+    services: {
+      auth: true, // Just as example. Can be any other service.
+      functions: {
+        location: 'asia-northeast1',
+        emulatorPort: 5001,
+        emulatorHost: 'http://localhost',
+      },
+    },
   },
   build: {},
 }
---
+++ plugins/vconsole.js
@@ -0,0 +1,3 @@
+import VConsole from 'vconsole'
+// eslint-disable-next-line
+const vConsole = new VConsole()
